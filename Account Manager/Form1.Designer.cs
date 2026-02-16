9ea10816b7b2c59bc9adbf943b33fdaae0fa3b7b
mataj <mataj.ardit@gmail.com>
2026-02-16 05:34:26 +0100

    Ajoutez des fichiers projet.

---
+++ Account Manager/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Account_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonsFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(726, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veuillez séléctionner un compte pour vous connecter:";
+            // 
+            // buttonsFlowLayoutPanel
+            // 
+            this.buttonsFlowLayoutPanel.AutoSize = true;
+            this.buttonsFlowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.buttonsFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.buttonsFlowLayoutPanel.Location = new System.Drawing.Point(29, 48);
+            this.buttonsFlowLayoutPanel.Name = "buttonsFlowLayoutPanel";
+            this.buttonsFlowLayoutPanel.Size = new System.Drawing.Size(0, 0);
+            this.buttonsFlowLayoutPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(789, 444);
+            this.Controls.Add(this.buttonsFlowLayoutPanel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Account Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel buttonsFlowLayoutPanel;
+    }
+}
+
---
+++ Account Manager/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += OnAccountManagerLoad;
+        }
+
+        private void OnAccountManagerLoad(object sender, EventArgs e)
+        {
+            string path = Path.Combine(Application.StartupPath, "accounts.json");
+
+            if(!File.Exists(path))
+            {
+                MessageBox.Show("Les données n'ont pas été trouvées, vous avez un problème avec le accounts.json");
+                return;
+            }
+
+            string json = File.ReadAllText(path);
+
+            List<AccountData> accountsDatas = JsonSerializer.Deserialize<List<AccountData>>(json);
+
+            CreateButtons(accountsDatas);
+        }
+
+        private void CreateButtons(List<AccountData> accountsDatas)
+        {
+            foreach (var account in accountsDatas)
+            {
+                Button btn = new Button();
+                btn.Text = account.Name;
+                btn.Size = new Size(150, 40);
+
+                btn.Tag = account;
+
+                btn.Click += ButtonClick;
+
+                buttonsFlowLayoutPanel.Controls.Add(btn);
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            AccountData accountData = btn.Tag as AccountData;
+
+            SendKeys.Send("%{TAB}");
+            SendKeys.Send(accountData.Username);
+            SendKeys.Send("{TAB}");
+            SendKeys.Send(accountData.Password);
+        }
+    }
+}
